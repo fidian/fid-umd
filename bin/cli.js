ff1082a94c00f4717e15cc84d6c17e8ce5689b00
Tyler Akins <fidian@rumkin.com>
2013-10-18 11:44:26 -0500

    Updates to compensate for jslint 0.2.0
    Changes that really affect this project:
     * Indentation must not use tabs
     * With empty catch blocks, the variable "ignore" should be used

--- bin/cli.js
+++ bin/cli.js
@@ -9,38 +9,38 @@ fs = require('fs');
 optionParser = require('OptionParser');
 parser = new optionParser.OptionParser();
 options = {
-	statusCode: 0
+    statusCode: 0
 };
 
 parser.addOption('h', 'help', 'Display this help message').action(function () {
-	console.log('Update or add UMD to JavaScript files');
-	console.log('');
-	console.log('Usage:');
-	console.log('\t' + parser.programName() + ' [options] filename [filename [...]]');
-	console.log('');
-	console.log('Available Options:');
-	console.log(parser.help());
-	console.log('');
-	console.log('If there are any files that have invalid JSON in their "fid-umd" markers,');
-	console.log('they will not be updated and this program will have a failure status code.');
-	process.exit(0);
+    console.log('Update or add UMD to JavaScript files');
+    console.log('');
+    console.log('Usage:');
+    console.log('\t' + parser.programName() + ' [options] filename [filename [...]]');
+    console.log('');
+    console.log('Available Options:');
+    console.log(parser.help());
+    console.log('');
+    console.log('If there are any files that have invalid JSON in their "fid-umd" markers,');
+    console.log('they will not be updated and this program will have a failure status code.');
+    process.exit(0);
 });
 
 function updateFile(filename) {
-	var contents, umd;
+    var contents, umd;
 
-	try {
-		umd = new FidUmd();
-		/*jslint stupid:true*/
-		contents = fs.readFileSync(filename).toString();
-		contents = umd.update(contents);
-		fs.writeFileSync(filename, contents);
-		/*jslint stupid:false*/
-	} catch (ex) {
-		console.log('Unable to update ' + filename);
-		console.log(ex.toString());
-		options.statusCode = 1;
-	}
+    try {
+        umd = new FidUmd();
+        /*jslint stupid:true*/
+        contents = fs.readFileSync(filename).toString();
+        contents = umd.update(contents);
+        fs.writeFileSync(filename, contents);
+        /*jslint stupid:false*/
+    } catch (ex) {
+        console.log('Unable to update ' + filename);
+        console.log(ex.toString());
+        options.statusCode = 1;
+    }
 }
 
 
@@ -48,8 +48,8 @@ filenames = parser.parse();
 filenames.forEach(updateFile);
 
 if (filenames.length === 0) {
-	console.error('Please pass filenames on the command line');
-	console.error('For a list of options, use --help');
+    console.error('Please pass filenames on the command line');
+    console.error('For a list of options, use --help');
 }
 
 process.exit(options.statusCode);
